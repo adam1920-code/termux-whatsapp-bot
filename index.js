80ce607b15661a48af7494c42bffd73b8a7215b4
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-01-03 10:51:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const qrcode = require("qrcode-terminal");
 const moment = require("moment");
 const cheerio = require("cheerio");
 const imageToBase64 = require('image-to-base64');
-const mp3 = require('mp3');
 const get = require('got')
 const fs = require("fs");
 const dl = require("./lib/downloadImage.js");
