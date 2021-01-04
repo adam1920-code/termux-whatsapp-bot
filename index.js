5608466a082cb1f2d86f177887146c358846ff0e
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-01-04 17:48:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const menu = require("./lib/menu.js")
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
 const BotName = 'YEONGSILL BOT'; 
-const instagram = 'Instagram kamu'; 
+const instagram = 'https://instagram.com/adam_oey107'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
